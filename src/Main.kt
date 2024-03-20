a633189d94842a521c506da80b59ae03cc034e36
Enrihh <laurilin2018@gmail.com>
2024-03-20 22:06:47 +0100

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+   val g: Long
+   val f: Float
+   g = 22
+   f = 2.2f
+   println(g*f)
+   println(g-f)
+   println(g/f)
+   println(g+f)
+   val g: Long
+   val f: Double
+   g = 22
+   f = 2.2
+   println(g-f)
+   println(g+f)
+   println(g/f)
+   println(g*f)
+}
